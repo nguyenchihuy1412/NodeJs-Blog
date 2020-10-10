988964da3767f752990184ed2c52717ab8b796e3
Nguyen Chi Huy <nguyenchihuy1412@gmail.com>
2020-10-10 19:16:31 +0700

    Clean Blog

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Clean Blog' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+/* GET post page. */
+router.get('/post', function(req, res, next) {
+  res.render('post', { title: 'Post' });
+});
+
+module.exports = router;
